17ae420e7d0b2d0f102d285f7223e23266e59c4b
Lev Vysokiy <visoko96.lev@mail.ru>
2019-06-27 00:09:51 +0500

    initial

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*- 
+import telebot as telebot
+from telebot import apihelper
+
+bot = telebot.TeleBot('301914397:AAEmR8WlfzyxQT53zdpqHrSwR8iwaKEr-h8')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=0)
